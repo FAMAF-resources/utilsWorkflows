f696e0cda26bd1e3d2c012fed499835f0f378327
shirosweets <valentinavispounc@gmail.com>
2023-08-05 21:51:33 -0300

    Update utils

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # How to use
 
 ```bash
-chmod +x rename_file.sh
+cd utilsWorkkflows
+```
+
+```bash
+chmod +x rename_file.sh update_w_from_parent.sh
+```
+
+```bash
+./update_w_from_parent.sh && ./rename_file.sh
 ```
--- update_w_from_parent.sh
+++ update_w_from_parent.sh
@@ -5,9 +5,9 @@ urlParentWorkflow='Por favor agregue el submodulo con el siguiente comando:\n\ng
 
 for file in $(ls -a); do
   if [ -d $file ] && [ $file = $folderToSearch ]; then
-    echo 'Archivo .github encontrado. Configurando utils para el workflow...'
-    cp utilsWorkflows/labeler.yml .github/
-    cp utilsWorkflows/list_auto_assign.yml .github/
+    echo 'Archivo .github encontrado. Configurando workflows...'
+    cp -r parentRepository/ useFolder
+    mv useFolder/ .github/workflows
     echo -e '\nConfiguracion completa.'
     exit 1
   fi
